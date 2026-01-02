cff8d15789e9ddb9579b11995e79afce510a814a
Adam Werner <adam.werner@castellantech.com>
2026-01-02 15:10:43 -0500

    Chore: Add a blank line for improved readability in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import viteImagemin from 'vite-plugin-imagemin';
 import { resolve } from 'path';
 
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
